bf9869ba764a4fd9da72fb2ec3dcb3cef3a2edc9
leesggoo <32794451+nperreau@users.noreply.github.com>
2026-06-07 10:09:33 -0700

    Added photo mode

---
+++ Assets/Scripts/Photo.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using JetBrains.Annotations;
+using SimpleFileBrowser;
+using UnityEngine;
+
+namespace MoleculeViewer
+{
+    public class Photo : MonoBehaviour
+    {
+        public List<GameObject> objectsToHide;
+
+        public void TakePhoto()
+        {
+            FileBrowser.SetFilters(true, new FileBrowser.Filter("Images", ".png"));
+            FileBrowser.SetDefaultFilter(".png");
+
+            FileBrowser.ShowSaveDialog(PerformTakePhoto, null, pickMode: FileBrowser.PickMode.Files);
+        }
+
+        void PerformTakePhoto([NotNull] string[] paths) => StartCoroutine(PhotoRoutine(paths.First()));
+
+        private IEnumerator PhotoRoutine(string path)
+        {
+            try
+            {
+                foreach (GameObject go in objectsToHide)
+                    if (go)
+                        go.SetActive(false);
+
+                yield return new WaitForEndOfFrame();
+                ScreenCapture.CaptureScreenshot(path);
+            }
+            finally
+            {
+                foreach (GameObject go in objectsToHide)
+                    if (go)
+                        go.SetActive(true);
+            }
+        }
+    }
+}
